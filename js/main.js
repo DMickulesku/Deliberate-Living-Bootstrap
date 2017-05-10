4e2e3e44ff7ea1829d0fcefc6b3c5b754c5dd28e
DMickulesku <danmickulesku@gmail.com>
2017-05-10 15:02:19 -0600

    most recent changes made to nav bar as well as social network links

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: 39.742043,
+        lng: -104.991531
+      },
+      zoom: 10
+    });
+
+  }
